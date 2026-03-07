937dd36f602278ade18d528e9bf081061710f3c4
Abdullah <bwazik@outlook.com>
2026-03-07 04:19:48 +0200

    fix: resolve attempt to read property gym on null in session manager

--- resources/views/livewire/session-manager.blade.php
+++ resources/views/livewire/session-manager.blade.php
@@ -28,7 +28,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                         </svg>
-                        <span>{{ $session->workoutIntent->gym->name }}</span>
+                        <span>{{ $session->workoutIntent?->gym?->name ?? 'جيم غير معروف' }}</span>
                     </div>
                 </div>
 
